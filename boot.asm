dedb8a7c588d5f5f5a4126882ce44f93e306823a
Ahmet Emin Seyfeli <219275782+AsCWE@users.noreply.github.com>
2026-06-18 10:01:21 +0300

    migrate storage to floppy disk architecture and fix BIOS registers
    - Switched disk loading from raw storage drive to standardized 1.44MB Floppy architecture (DL = 0x00).
    - Successfully validated segment returns and completed sector 2 memory reads without hanging the CPU.

--- boot.asm
+++ boot.asm
@@ -1,39 +1,64 @@
 [BITS 16]
 [ORG 0x7C00]
+
 start:
-xor ax,ax 
-mov ds,ax
 
-mov si, TEXT
-call PrintString
+ xor ax,ax
+ mov ds,ax
 
-jmp $
+ mov si, welcome
+ call PrintString
 
+ mov bx, 0x8000
+ call ReadDisk
+ mov si, dsuccess
+ call PrintString
+ jmp $
+
+ReadDisk:
+
+ mov ah, 0x02
+ mov al, 1
+ mov ch, 0
+ mov cl, 2
+ mov dh, 0
+ mov dl, 0x00
+ int 0x13
+ jc DiskError
+ ret
+
+DiskError:
+ mov si, derror
+ call PrintString
 
 PrintChar:
-mov ah, 0x0E
-mov bh, 0x00
-mov bl, 0x07
 
-INT 0x10
-ret
+ mov ah, 0x0E
+ mov bh, 0x00
+ mov bl, 0x07
+ int 0x10
+ ret
+
 
 
 PrintString:
 
 NextChar:
-mov al, [si]
-inc si
-or al,al
-jz exit
-call PrintChar
-jmp NextChar
+ mov al, [si]
+ inc si
+ or al,al
+ jz exit
+ call PrintChar
+ jmp NextChar
 
 exit:
-ret
+ ret
 
 
-TEXT db 'AsCore 0.0.1 booting...', 0
-
+welcome db 'Welcome to AsCore!',13 ,10 ,0
+derror db 'Failed to read from sector',13 ,10 ,0
+dsuccess db 'Reading sector 2 success',13, 10 ,0
 times 510 - ($ - $$) db 0
 dw 0xAA55
+
+times 512 db 0xDE
