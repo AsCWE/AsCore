644fff2adf7dfcd5ec962c7634297e9e2cf43885
Ahmet Emin Seyfeli <219275782+AsCWE@users.noreply.github.com>
2026-06-20 07:48:58 +0300

    implement screen clearing and basic string printing in 16-bit C

--- kernel.c
+++ kernel.c
@@ -1,20 +1,39 @@
 __asm__(".code16gcc\n");
 
-void kernel_main(){
-
 volatile char* video_mem = (char*)0xB8000;
+int cursor_x = 0;
+int cursor_y = 0;
+
+void kernel_main(){
 
 for (int i = 0; i < 80 * 25; i++) {
   video_mem[i * 2] = ' ';
-  video_mem[i * 2 + 1] = 0x07;
+  video_mem[i * 2 + 1] = 0x0A;
 }
 
-video_mem[0] = 'A';
-video_mem[1] = 0x02;
+void print(char *str);
+
+print("Welcome to AsCore!\n");
 
 while(1){
-
 __asm__ volatile("hlt");
+}
+}
 
+void print(char *str){
+
+for(; *str != '\0'; str++){
+
+if(*str == '\n'){
+cursor_x = 0;
+cursor_y++;
+}
+else{
+
+int offset = (cursor_y * 80 + cursor_x) * 2;
+video_mem[offset] = *str;
+video_mem[offset + 1] = 0x0A;
+cursor_x++;
+}
 }
 }
