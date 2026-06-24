c92e626dc726f31daf25eb573c0b690555d0b608
Ahmet Emin Seyfeli <219275782+AsCWE@users.noreply.github.com>
2026-06-24 06:59:14 +0300

    Modify build.sh for 16-bit kernel compilation
    Updated build script to compile kernel and related files with 16-bit mode and link with kernel entry.

--- build.sh
+++ build.sh
@@ -3,11 +3,16 @@
 echo "[+] Compiling Bootloader"
 nasm -f bin boot.asm -o boot.bin
 
+echo "[+] Compiling Kernel Entry..."
+nasm -f elf32 entry.asm -o kernel_entry.o
+
 echo "[+] Compiling kernel..."
-gcc -m32 -ffreestanding -fno-pie -fno-pic -c kernel.c -o kernel.o
+gcc -m16 -ffreestanding -fno-pie -fno-pic -fno-asynchronous-unwind-tables -march=i386 -c kernel.c -o kernel.o
+gcc -m16 -ffreestanding -fno-pie -fno-pic -fno-asynchronous-unwind-tables -march=i386 -c io.c -o io.o
+gcc -m16 -ffreestanding -fno-pie -fno-pic -fno-asynchronous-unwind-tables -march=i386 -c vga.c -o vga.o
 
 echo "[+] Linking binaries..."
-ld -m elf_i386 -Ttext 0x7E00 -e kernel_main --oformat binary kernel.o -o kernel.bin
+ld -m elf_i386 -Ttext 0x7E00 -e _start --oformat binary -nostdlib kernel_entry.o kernel.o vga.o io.o -o kernel.bin
 
 echo "[+] Creating 1.44 MB Floppy image..."
 dd if=/dev/zero of=ascore.img bs=1024 count=1440
