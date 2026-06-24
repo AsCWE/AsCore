6b72029ea67fbf0225d42d23656abcd633745baa
Ahmet Emin Seyfeli <219275782+AsCWE@users.noreply.github.com>
2026-06-24 06:59:56 +0300

    Refactor kernel_main and clean up print function
    Refactor kernel_main to include clear_screen and print functions, and remove redundant code.

--- kernel.c
+++ kernel.c
@@ -1,39 +1,14 @@
 __asm__(".code16gcc\n");
 
-volatile char* video_mem = (char*)0xB8000;
-int cursor_x = 0;
-int cursor_y = 0;
+#include "vga.h"
 
 void kernel_main(){
+    clear_screen();
+    print("Welcome to AsCore!\n");
 
-for (int i = 0; i < 80 * 25; i++) {
-  video_mem[i * 2] = ' ';
-  video_mem[i * 2 + 1] = 0x0A;
-}
+ while(1){
 
-void print(char *str);
+    __asm__ volatile("hlt");
 
-print("Welcome to AsCore!\n");
-
-while(1){
-__asm__ volatile("hlt");
-}
-}
-
-void print(char *str){
-
-for(; *str != '\0'; str++){
-
-if(*str == '\n'){
-cursor_x = 0;
-cursor_y++;
-}
-else{
-
-int offset = (cursor_y * 80 + cursor_x) * 2;
-video_mem[offset] = *str;
-video_mem[offset + 1] = 0x0A;
-cursor_x++;
-}
-}
+  }
 }
