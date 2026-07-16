e25400db707d269b3d0c636324c669c31865e6e6
Ahmet Emin Seyfeli <219275782+AsCWE@users.noreply.github.com>
2026-07-16 12:49:45 +0300

    .

--- kernel.c
+++ kernel.c
@@ -4,7 +4,10 @@
 void kernel_main(void){
 
     uart_print("AsCore Kernel Booted Successfully!\n");
-    *(volatile uint8_t *)(0x10000000 + 1) =0x10;
+    *(volatile uint8_t *)(0x10000000 + 1) = 0x01;
+    *(volatile uint32_t *)(0x0C000000 + (10 * 4)) = 1;
+    *(volatile uint32_t *)(0x0C002000) = (1 << 10);
+    *(volatile uint32_t *)(0x0C200000) = 0;
     asm volatile("csrs mie, %0" :: "r"(1 << 11));
     asm volatile("csrs mstatus, %0" :: "r"(1 << 3));
     uart_print("AsCore: waiting for Keyboard Interrupt.\n");
