329b65f2db62b2015774c8d40fc23cb349c05488
Ahmet Emin Seyfeli <219275782+AsCWE@users.noreply.github.com>
2026-07-17 19:52:08 +0300

    Refactor kernel_main and add init_hardware function
    adding shell

--- kernel.c
+++ kernel.c
@@ -1,17 +1,23 @@
 #include "uart.h"
-#include<stdint.h>
+#include <stdint.h>
+#include "shell.h"
 
-void kernel_main(void){
+static inline void init_hardware(void){
 
-    uart_print("AsCore Kernel Booted Successfully!\n");
     *(volatile uint8_t *)(0x10000000 + 1) = 0x01;
     *(volatile uint32_t *)(0x0C000000 + (10 * 4)) = 1;
     *(volatile uint32_t *)(0x0C002000) = (1 << 10);
     *(volatile uint32_t *)(0x0C200000) = 0;
     asm volatile("csrs mie, %0" :: "r"(1 << 11));
     asm volatile("csrs mstatus, %0" :: "r"(1 << 3));
-    uart_print("AsCore: waiting for Keyboard Interrupt.\n");
-    asm volatile("ecall");
+}
+
+
+void kernel_main(void){
+    init_hardware();
+    uart_print("AsCore Kernel Booted Successfully!\n");
+    print_banner();
+    print_prompt();
 
     while(1){
 
