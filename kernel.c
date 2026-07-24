2db443f1deab5e2e02a583692507434adec4c008
Ahmet Emin Seyfeli <219275782+AsCWE@users.noreply.github.com>
2026-07-24 12:08:40 +0300

    Initialize tasks and IPC in kernel_main
    Added task and IPC initialization to kernel main.

--- kernel.c
+++ kernel.c
@@ -1,6 +1,14 @@
 #include "uart.h"
 #include <stdint.h>
 #include "shell.h"
+#include "task.h"
+#include "ipc.h"
+
+extern volatile int cmd_ready;
+extern volatile int cmd_idx;
+extern char cmd_buffer[];
+extern void read_command(char *cmd);
+
 
 static inline void init_hardware(void){
 
@@ -13,13 +21,28 @@ static inline void init_hardware(void){
 }
 
 
+
+
 void kernel_main(void){
     init_hardware();
+
+    task_init();
+    current_task = task_create(PID_SHELL, "shell");
+    current_task->state = TASK_RUNNING;
+
+    task_create(PID_ENCRYPT_NET, "encrypt_net");
+
     uart_print("AsCore Kernel Booted Successfully!\n");
+
     print_banner();
     print_prompt();
 
     while(1){
-
+        if(cmd_ready){
+            read_command(cmd_buffer);
+            cmd_idx = 0;
+            cmd_ready = 0;
+            print_prompt();
+        }
     }
 }
