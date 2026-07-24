d4f7d0220b217466c2b3ad86ba3e9f88faf80d29
Ahmet Emin Seyfeli <219275782+AsCWE@users.noreply.github.com>
2026-07-24 12:09:28 +0300

    Refactor trap_handler to add arguments and debug output
    Updated trap_handler to include additional arguments and debug messages.

--- trap.c
+++ trap.c
@@ -1,10 +1,11 @@
 #include <stdint.h>
 #include "uart.h"
 #include "shell.h"
+#include "ipc.h"
 
 extern void uart_print(const char *str);
 
-void trap_handler(void){
+void trap_handler(uint32_t arg0, uint32_t arg1, uint32_t arg2){
     uint32_t mcause;
     uint32_t mepc;
 
@@ -15,7 +16,7 @@ void trap_handler(void){
         uint32_t int_id = mcause & 0x7FFFFFFF;
 
         if (int_id == 7) {
-            uart_print("AsCore: Timer interrupt received.\n");
+            uart_print("[Debug]: Timer interrupt received.\n");
         }
         else if (int_id == 11){
             volatile uint32_t *plic_claim =(volatile uint32_t *)0x0C200004;
@@ -30,19 +31,21 @@ void trap_handler(void){
             *plic_claim = irq;
         }
         else{
-            uart_print("AsCore: Unknown external hardware interrupt received.\n");
+            uart_print("[Debug]: Unknown external hardware interrupt received.\n");
         }
     }
     else {
         uint32_t exc_id = mcause & 0x7FFFFFFF;
 
-        if (exc_id == 11) {
-            uart_print("AsCore: Syscall (ecall) received!\n");
+        if(exc_id == 11 || exc_id == 8 || exc_id == 9 || exc_id == 10){
+            uart_print("[Debug]: Syscall (ecall) received!\n");
+            uint32_t res = ipc_handler(arg0, arg1, arg2);
+            asm volatile ("mv a0, %0" :: "r"(res));
             mepc += 4;
             asm volatile("csrw mepc, %0" :: "r"(mepc));
         }
         else {
-            uart_print("AsCore: Kernel Panic !\n");
+            uart_print("[Debug]: Kernel Panic !\n");
             while(1);
         }
     }
