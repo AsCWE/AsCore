4c7793bf6dc42cbdd3f46ad465e9903aac727c09
Ahmet Emin Seyfeli <219275782+AsCWE@users.noreply.github.com>
2026-07-24 12:11:01 +0300

    Implement uart_printhex for hexadecimal output
    Added uart_printhex function to print hexadecimal values.

--- uart.c
+++ uart.c
@@ -1,4 +1,5 @@
 #include "uart.h"
+#include <stdint.h>
 #define UART_THR ((volatile char*)0x10000000)
 #define UART_LSR ((volatile char*)0x10000005)
 
@@ -15,6 +16,21 @@ void uart_print(const char *str){
     for(; *str != '\0'; str++){
 
         uart_putc(*str);
+    }
+
+}
+void uart_printhex(uint32_t val){
+
+    uart_print("0x");
+
+    for(int i = 7; i >= 0; i--){
+        uint8_t nibble = (val >> (i * 4)) & 0xF;
+        if(nibble < 10){
+            uart_putc('0'+ nibble);
+        }
+        else{
+            uart_putc('A' + (nibble - 10));
+        }
     }
 
 }
