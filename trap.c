abce746c8f1be41242d42aba961383fa4d1f0517
AsCWE <219275782+AsCWE@users.noreply.github.com>
2026-08-06 11:47:21 +0300

    Enhance trap_handler with debug prints
    Added debug prints for various interrupt and syscall events when DEBUG_TRAPS is not defined.

--- trap.c
+++ trap.c
@@ -3,6 +3,8 @@
 #include "shell.h"
 #include "ipc.h"
 
+#define DEBUG_TRAPS
+
 extern void uart_print(const char *str);
 
 void trap_handler(uint32_t arg0, uint32_t arg1, uint32_t arg2){
@@ -16,7 +18,9 @@ void trap_handler(uint32_t arg0, uint32_t arg1, uint32_t arg2){
         uint32_t int_id = mcause & 0x7FFFFFFF;
 
         if (int_id == 7) {
+#ifndef DEBUG_TRAPS
             uart_print("[Debug]: Timer interrupt received.\n");
+#endif
         }
         else if (int_id == 11){
             volatile uint32_t *plic_claim =(volatile uint32_t *)0x0C200004;
@@ -31,21 +35,31 @@ void trap_handler(uint32_t arg0, uint32_t arg1, uint32_t arg2){
             *plic_claim = irq;
         }
         else{
+#ifndef DEBUG_TRAPS
             uart_print("[Debug]: Unknown external hardware interrupt received.\n");
+#endif
         }
     }
     else {
         uint32_t exc_id = mcause & 0x7FFFFFFF;
 
         if(exc_id == 11 || exc_id == 8 || exc_id == 9 || exc_id == 10){
+
+#ifndef DEBUG_TRAPS
             uart_print("[Debug]: Syscall (ecall) received!\n");
+#endif
+
             uint32_t res = ipc_handler(arg0, arg1, arg2);
             asm volatile ("mv a0, %0" :: "r"(res));
             mepc += 4;
             asm volatile("csrw mepc, %0" :: "r"(mepc));
         }
         else {
-            uart_print("[Debug]: Kernel Panic !\n");
+            uart_print("[Debug]: Kernel Panic! exc_id = ");
+            uart_printhex(exc_id);
+            uart_print(" | mepc = ");
+            uart_printhex(mepc);
+            uart_print("\n");
             while(1);
         }
     }
